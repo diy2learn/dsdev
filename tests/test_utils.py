8540329b2e3d998311a7477b2f67fb9064682583
diy2learn <bian.tquang@gmail.com>
2021-08-04 07:58:42 +0200

    add test_get_get_configs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,7 @@ class TestUtil:
     def test_gen_repo_url(self):
         res = utils.gen_repo_url("git_account", "git_pat", "repo_name")
         assert res == "https://git_pat@github.com/git_account/repo_name.git"
+
+    def test_get_get_configs(self):
+        res = utils.get_get_configs()
+        assert "GIT_ACCOUNT" in res.keys()
