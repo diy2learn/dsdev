33e20ccea3c73a6b62a50571d3df648f9377a561
diy2learn <bian.tquang@gmail.com>
2021-08-06 05:21:23 +0200

    add docs: git_clone

--- src/dsdev/tasks.py
+++ src/dsdev/tasks.py
@@ -13,6 +13,28 @@ def git_clone(ctx, pkg_name, git_account_alias=None, dry_run="n"):
     """
     Clone a git repo.
     Git account and PAT need to be provided in a configure file.
+
+    Parameters
+    ----------
+    pkg_name: str, name of the required package.
+    git_account_alias: str [Optional], default to None
+        alias of the git_account in the `.dsdev.cfg` file.
+        if None, will take the default alias.
+    dry_run: str, default to "n"
+        in dry_run mode or not.
+
+    Examples
+    --------
+    dry-run:
+    $ dsdev git-clone -p test -g test_account -d y
+    ... git clone https://test_pat@github.com/test_account/test.git
+
+     $ dsdev git-clone -p test -d y
+    ... git clone https://***@github.com/***/test.git
+
+    real run:
+    $ dsdev git-clone -p <package_name>
+    ... cloning into <package_name> ...
     """
     git_account_alias = (
         git_account_alias if git_account_alias else defs.GIT_ACCOUNT_ALIAS_DEFAULT
