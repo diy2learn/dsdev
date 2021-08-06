ad27269fcb10d3a347d9b53eec8f41ee71808772
diy2learn <bian.tquang@gmail.com>
2021-08-06 06:05:38 +0200

    add docs: get_get_configs

--- src/dsdev/utils.py
+++ src/dsdev/utils.py
@@ -9,7 +9,19 @@ def gen_repo_url(git_account: str, git_pat: str, repo_name: str):
     return f"https://{git_pat}@github.com/{git_account}/{repo_name}.git"
 
 
-def get_get_configs(fpath: str = None):
+def get_get_configs(fpath: str = None) -> dict:
+    """
+    Get configs from a configure file.
+
+    Parameters
+    ----------
+    fpath: str[Optional]
+        if not provide, will search for a default configure file.
+
+    Returns
+    -------
+    dict
+    """
     fpath = (
         fpath if fpath else os.getenv("DSDEV_CONFIG_PATH", DSDEV_CONFIG_PATH_DEFAULT)
     )
