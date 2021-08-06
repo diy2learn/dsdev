a087c7ae14c195a86eec35416c02d8a43167e9f0
diy2learn <bian.tquang@gmail.com>
2021-08-06 06:07:53 +0200

    add docs: gen_repo_url

--- src/dsdev/utils.py
+++ src/dsdev/utils.py
@@ -5,7 +5,23 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 DSDEV_CONFIG_PATH_DEFAULT = f"{HERE}/.dsdev.cfg"
 
 
-def gen_repo_url(git_account: str, git_pat: str, repo_name: str):
+def gen_repo_url(git_account: str, git_pat: str, repo_name: str) -> str:
+    """
+    Generate a url to a git repo.
+
+    Parameters
+    ----------
+    git_account: str
+        git account
+    git_pat: str
+        Personal Access Token (PAT)
+    repo_name: str
+        name of the repo.
+
+    Returns
+    -------
+    str
+    """
     return f"https://{git_pat}@github.com/{git_account}/{repo_name}.git"
 
 
