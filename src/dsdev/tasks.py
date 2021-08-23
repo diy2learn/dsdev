33e352b75febf312efbd31e7dcb31ec9383ce6e3
diy2learn <bian.tquang@gmail.com>
2021-08-23 09:25:29 +0200

    refactor git_clone() with get_configs_item()

--- src/dsdev/tasks.py
+++ src/dsdev/tasks.py
@@ -39,10 +39,21 @@ def git_clone(ctx, pkg_name, git_account_alias=None, dry_run="n"):
     git_account_alias = (
         git_account_alias if git_account_alias else defs.GIT_ACCOUNT_ALIAS_DEFAULT
     )
+    configs_fpath = utils.get_config_fpath()
     configs = utils.get_configs()
-    git_configs = configs[defs.GIT_CONFIGS][git_account_alias]
-    git_account = git_configs[defs.GIT_ACCOUNT_LABEL]
-    git_pat = git_configs[defs.GIT_PAT_LABEL]
+    # git_configs_keypath = f"{defs.GIT_CONFIGS}/{git_account_alias}"
+    # git_configs = utils.get_configs_item(configs, git_configs_keypath, configs_fpath)
+    # git_configs = configs[defs.GIT_CONFIGS][git_account_alias]
+    # git_account = git_configs[defs.GIT_ACCOUNT_LABEL]
+    # git_pat = git_configs[defs.GIT_PAT_LABEL]
+
+    git_account_keypath = (
+        f"{defs.GIT_CONFIGS}/{git_account_alias}/{defs.GIT_ACCOUNT_LABEL}"
+    )
+    git_pat_keypath = f"{defs.GIT_CONFIGS}/{git_account_alias}/{defs.GIT_PAT_LABEL}"
+    git_account = utils.get_configs_item(configs, git_account_keypath, configs_fpath)
+    git_pat = utils.get_configs_item(configs, git_pat_keypath, configs_fpath)
+
     url = utils.gen_repo_url(git_account, git_pat, pkg_name)
     print(f"[INFO] clonning the package {pkg_name}")
     cmd = f"git clone {url}"
